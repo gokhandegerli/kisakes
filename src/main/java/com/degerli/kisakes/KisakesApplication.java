59223712d05e22e1fa30da2b2927a8852aca36d3
gokhandegerli <gokhan.degerli.sd@gmail.com>
2025-11-30 15:20:56 +0300

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/degerli/kisakes/KisakesApplication.java
@@ -0,0 +1,13 @@
+package com.degerli.kisakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KisakesApplication.class, args);
+	}
+
+}
